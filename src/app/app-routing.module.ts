0b15f2b4fdad57985822336d526072569a973b72
Ansof Habibunnadjar <ansofhn5@gmail.com>
2023-01-03 17:56:36 +0700

    feat: add pages content, product content, n routing for navbar n each component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'user', component: UserComponent },
+  { path: 'product', component: ProductsComponent },
+  { path: 'pages', component: PagesComponent },
+  { path: '', component: DashboardComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import { ProductsComponent } from './products/products.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, DashboardComponent, UserComponent],
+  declarations: [AppComponent, NavbarComponent, DashboardComponent, UserComponent, ProductsComponent, PagesComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
